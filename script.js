c1f10956ae450e716b9285230c51b762f5a37655
Sarthak <sarthakdhoke132@gmail.com>
2023-06-24 12:04:53 +0530

    replace resume file

---
+++ script.js
@@ -0,0 +1,9 @@
+// const scriptURL = 'https://script.google.com/macros/s/AKfycbw5S1DR1248Lk32VGAgOZBJZ_Jrg8CkJVf7eAWtMXLQ4w0lJQqC40z2lkxmdwMkXx9-BQ/exec'
+// const form = document.forms['google-sheet']
+
+// form.addEventListener('submit', e => {
+//   e.preventDefault()
+//   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+//     .then(response => alert("Thanks for Contacting us..! We Will Contact You Soon..."))
+//     .catch(error => console.error('Error!', error.message))
+// prompt("How old are you?");
